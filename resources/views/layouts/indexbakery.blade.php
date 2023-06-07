333ad776b31558fec0ff3f8403af0a1cc2ac704e
Mamluatul Haniah <mamluatulhaniah@polinema.ac.id>
2023-06-07 11:54:44 +0700

    push by mmh

--- resources/views/layouts/indexbakery.blade.php
+++ resources/views/layouts/indexbakery.blade.php
@@ -470,11 +470,6 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <!-- End Footer -->
-  @include('layouts.user.footer')
-  <!-- End Footer -->
-
   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <div id="preloader"></div>
